9bf6e75aaef4bb1acdfe3d95c0554b02945d2577
WRKMAN-CRYPTO <workinmancrypto@gmail.com>
2026-08-08 12:42:24 -0400

    Remove temporary GRID BREAKER cabinet patch script

--- scripts/patch_wip_grid_breaker.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-p=Path('index.html')
-s=p.read_text()
-s=s.replace('data-title="WIP // VEIL//GEAR"','data-title="WIP // GRID//BREAKER"',1)
-s=s.replace('WIP//<br>VEIL GEAR','WIP//<br>GRID BREAKER',1)
-s=s.replace('<div class="symbol">⚙</div><small>HUMANOID FRAME • RESONANCE DRIVE</small>','<div class="symbol">✕</div><small>DECOMPOSE • CHARGE • COMBINE</small>',1)
-p.write_text(s)
